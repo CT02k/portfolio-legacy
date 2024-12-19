a4bd5f88775d43bc2ebd7f3e2ce4812adbc41be6
CT02k <joaopaulocfwork@gmail.com>
2024-12-19 04:57:47 -0300

    Inicial

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,16 @@
+export default function Page() {
+    return (
+      <main className="content">
+        <section className="main">
+          <h1>Contato<span>*</span></h1>
+          <p>
+            Entre em contato comigo através do email <a className="link" href="mailto:contato@ct02.work">contato@ct02.work</a>
+          </p>
+          <p>
+            Ou pelo Discord <a className="link" href="https://discord.com/users/1292489217464860713">@ellestk</a>
+          </p>
+        </section>
+      </main>
+    );
+  }
+  
---
+++ src/app/projects/page.tsx
@@ -0,0 +1,54 @@
+export default function Page() {
+    return (
+      <main className="content">
+        <section className="main">
+          <h1>Projetos<span>*</span></h1>
+          <p>Conheça alguns dos projetos que desenvolvi</p>
+          <div className="project-group">
+            <a href="">         
+              <div className="project">
+                <img src="/rs.jpg" alt="Projeto 1" />
+                <div className="text">
+                  <h2>RedSilver</h2>
+                  <p>Loja de panelas online pra aplicar golpe em trouxa, faz o L</p>
+                </div>
+              </div>
+            </a>
+            <hr />
+            <a href="">         
+              <div className="project">
+                <img src="/rs.jpg" alt="Projeto 1" />
+                <div className="text">
+                  <h2>RedSilver</h2>
+                  <p>Loja de panelas online pra aplicar golpe em trouxa, faz o L</p>
+                </div>
+              </div>
+            </a>
+            <hr />
+            <a href="">         
+              <div className="project">
+                <img src="/rs.jpg" alt="Projeto 1" />
+                <div className="text">
+                  <h2>RedSilver</h2>
+                  <p>Loja de panelas online pra aplicar golpe em trouxa, faz o L</p>
+                </div>
+              </div>
+            </a>
+            <hr />
+            <a href="">         
+              <div className="project">
+                <img src="/rs.jpg" alt="Projeto 1" />
+                <div className="text">
+                  <h2>RedSilver</h2>
+                  <p>Loja de panelas online pra aplicar golpe em trouxa, faz o L</p>
+                </div>
+              </div>
+            </a>
+            <hr />
+            
+          </div>
+        </section>
+      </main>
+    );
+  }
+  
