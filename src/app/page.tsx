f1a90afb478feefe26f3944a222f5c31ce85f9d9
João Paulo <130176216+CT02k@users.noreply.github.com>
2025-04-26 20:19:20 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,6 @@
 "use client";
 
-import { useRouter } from "next/navigation";
-
 export default function Home() {
-  const router = useRouter();
-
-  const handleNavigation = (path: string) => {
-    router.push(path);
-  };
-
   return (
     <main className="content">
     </main>
