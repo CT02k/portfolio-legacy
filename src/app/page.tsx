a389ccf941133fbc9bee0bead46dfae9875a187d
João Paulo <130176216+CT02k@users.noreply.github.com>
2025-04-26 20:16:57 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,19 +11,6 @@ export default function Home() {
 
   return (
     <main className="content">
-      <section className="main">
-        <h1 className="name">
-          CT02<span>*</span>
-        </h1>
-        <p className="description">
-          Sou um desenvolvedor apaixonado por tecnologia e inovação. Atualmente sou um desenvolvedor fullstack, com foco em desenvolvimento web. Trabalho com tecnologias como React, Next.js, Node.js, TypeScript, entre outras.
-        </p>
-        <div className="buttons">
-          <button onClick={() => handleNavigation("/projects")} className="cta">Projetos</button>
-          <button onClick={() => handleNavigation("/habilidades")} className="cta">Habilidades</button>
-          <button onClick={() => handleNavigation("/contact")} className="cta">Contato</button>
-        </div>
-      </section>
     </main>
   );
 }
