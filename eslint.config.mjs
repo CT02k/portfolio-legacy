817869f52d5de18b4d272068c1d33f7a38dbc9c4
CT02k <joaopaulocfwork@gmail.com>
2025-06-24 19:01:26 -0300

    Bug fixes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,7 +11,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   {
-    ignores: ["src/generated/prisma/**"], // Ignora os arquivos Prisma
+    ignores: ["src/generated/prisma/**"],
   },
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
